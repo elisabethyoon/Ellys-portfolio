011d549e9cd3d531fe2264f47d6fba25f48abc61
“elesabeth” <careerwoman87@gmail.com>
2022-11-25 17:01:02 +0900

    update: slider animation

--- resources/js/common-ui.js
+++ resources/js/common-ui.js
@@ -118,7 +118,7 @@
     const swiper = new Swiper('.mySwiper', {
       slidesPerView: 'auto',
       centeredSlides: true,
-      spaceBetween: 50,
+      spaceBetween: 0,
       // loop: true,
       pagination: {
         el: '.swiper-pagination',
