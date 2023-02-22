cf886ef75f2ca0500520e4cd7ae960946cd0c1e2
“elesabeth” <careerwoman87@gmail.com>
2023-02-22 17:21:31 +0900

    gsap animation project update

--- resources/js/common-ui.js
+++ resources/js/common-ui.js
@@ -23,14 +23,15 @@
     // circleEffect
     main_circle_effect();
     // textillate animation
-    contactTextAnimation();
     mainTitleAnimation();
+    contactTextAnimation();
     // scrollIcon
     scrollIcon();
+    // gsap popup
+    animationEffect();
   });
 
   // ------------------------ ui 함수 -----------------------------//
-
   var ep = null;
   var flag = false;
   function fullScreenPopup() {
@@ -41,7 +42,7 @@
       var idx = $(this)
         .closest('.pj-item')
         .attr('data-index');
-      mainSwiper.autoplay.stop();
+      // mainSwiper.autoplay.stop();
       flag = true;
 
       fullScreenPopup.addClass('active');
@@ -52,9 +53,9 @@
         .addClass('active');
 
       $('body').addClass('scroll-disable');
-
+      ScrollTrigger.refresh(true);
       ep = setTimeout(() => {
-        fullScreenPopup.find('.project-section > .side').addClass('active');
+        fullScreenPopup.find('.project-section > .side, .project-section .contents-area').addClass('active');
       }, 600);
     });
   }
@@ -63,13 +64,13 @@
     const fullScreenPopup = $('.full-screen-area');
 
     fullScreenPopup.find('.btn-popup').click(function() {
-      mainSwiper.autoplay.start();
+      // mainSwiper.autoplay.start();
       flag = false;
       fullScreenPopup.removeClass('active');
       fullScreenPopup.find('.wrap-contents').removeClass('active');
       $('body').removeClass('scroll-disable');
 
-      fullScreenPopup.find('.project-section > .side').removeClass('active');
+      fullScreenPopup.find('.project-section > .side, .project-section .contents-area').removeClass('active');
 
       clearTimeout(ep);
     });
@@ -172,7 +173,6 @@
       var st01 = $(e).find('.st01');
       var profileImg = $(e).find('.profile-img');
       var mask = $(e).find('.mask');
-
       var tl = gsap.timeline({
         scrollTrigger: {
           trigger: e,
@@ -184,7 +184,6 @@
         }
       });
       tl.to(st01, { x: 0, opacity: 1, ease: Expo.out, duration: 1.5 }, 0.5);
-
       tl.to(mask, { scaleX: 0, duration: 1.5, ease: Expo.out }, 0.4);
       tl.from(profileImg, { scale: 1.3, duration: 2.5 }, 0.4);
     });
@@ -220,6 +219,129 @@
     });
   }
 
+  // gsap animation project
+  function animationEffect() {
+    //// 마크업 순서대로 scrolltrigger도 순서에 맞게 배치해야함
+
+    let effectLine = $('.effect-line');
+    let fixTitle = $('.intro-fix-title');
+    let card02 = $('.card-item02');
+    let card03 = $('.card-item03');
+    let card04 = $('.card-item04');
+    let comparisonImage01 = $('.afterImage');
+    let comparisonImage02 = $('.afterImage img');
+    let view01 = $('.seperate01');
+    let view02 = $('.seperate02');
+    let view03 = $('.seperate03');
+
+    ////// intro line & title 효과
+    let tl1 = gsap.timeline();
+    ScrollTrigger.create({
+      animation: tl1,
+      trigger: '.intro-fix-wrap',
+      scroller: '.wrap-contents-container',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      pin: true,
+      pinSpacing: false
+    });
+
+    tl1.to(fixTitle, {
+      scale: 1.5,
+      ease: 'cubic-bezier(0.25, 0.8, 0.25, 1)',
+      duration: 1,
+      opacity: 0
+    });
+    tl1.from(effectLine, {
+      scaleX: 0,
+      transformOrigin: 'center center',
+      ease: 'none',
+      duration: 1.5,
+      delay: -1
+    });
+
+    ////// title pinning 효과
+    gsap.utils.toArray('.panel').forEach((panel, i) => {
+      ScrollTrigger.create({
+        trigger: panel,
+        scroller: '.wrap-contents-container',
+        start: 'top top',
+        pin: true,
+        pinSpacing: false,
+        anticipatePin: 1
+      });
+    });
+
+    ////// 3등분 효과
+    let tl3 = gsap.timeline();
+    ScrollTrigger.create({
+      animation: tl3,
+      trigger: '.section-seperate-effect',
+      scroller: '.wrap-contents-container',
+      start: 'top top',
+      end: () => '+=' + innerHeight * 3,
+      scrub: true,
+      pin: true
+    });
+
+    tl3
+      .fromTo(view01, { minWidth: '100%', transformOrigin: '0 100% 0', duration: 0.8 }, { minWidth: 'calc(33.33% - 8px)', transformOrigin: '0 0 0' })
+      .fromTo(view02, { minWidth: '100%', transformOrigin: '0 100% 0', duration: 0.8 }, { minWidth: 'calc(33.33% - 8px)', transformOrigin: '0 0 0' })
+      .fromTo(view03, { minWidth: '50%', transformOrigin: '0 100% 0', duration: 0.8 }, { minWidth: 'calc(33.33% - 8px)', transformOrigin: '0 0 0' });
+
+    ////// 스크롤 시 이미지 컬러로 채워지는 효과
+    let tl4 = gsap.timeline();
+    ScrollTrigger.create({
+      animation: tl4,
+      trigger: '.comparisonSection',
+      scroller: '.wrap-contents-container',
+      start: 'top top',
+      end: () => '+=' + innerHeight * 2.5,
+      scrub: true,
+      pin: true
+    });
+    // animate the container one way...
+    tl4.fromTo(comparisonImage01, { xPercent: 100, x: 0 }, { xPercent: 0 });
+    // ...and the image the opposite way (at the same time)
+    tl4.fromTo(comparisonImage02, { xPercent: -100, x: 0 }, { xPercent: 0 }, 0);
+
+    ////// 카드 순서대로 커지는 효과
+    let tl2 = gsap.timeline();
+    ScrollTrigger.create({
+      animation: tl2,
+      trigger: '.section-card-effect',
+      scroller: '.wrap-contents-container',
+      start: 'top top',
+      end: '+=2000',
+      scrub: true,
+      pin: true
+    });
+    var duration = 0.7;
+    var fromOption = {
+      scale: 0,
+      opacity: 0,
+      transformOrigin: '0 0 0',
+      ease: 'cubic-bezier(0.25, 0.8, 0.25, 1)'
+    };
+
+    function toOption(index) {
+      var option = {
+        scale: 1,
+        opacity: 1,
+        ease: 'cubic-bezier(0.25, 0.8, 0.25, 1)',
+        duration: duration * index,
+        transformOrigin: '0 100% 0'
+      };
+
+      return option;
+    }
+    tl2
+      .fromTo(card02, fromOption, toOption(1.5))
+      .fromTo(card03, fromOption, toOption(1))
+      .fromTo(card04, fromOption, toOption(1.5));
+  }
+
   // circle effect
   function main_circle_effect() {
     setTimeout(function() {
@@ -242,11 +364,11 @@
       centeredSlides: true,
       spaceBetween: 0,
       loop: true,
-      autoplay: {
-        delay: 4000,
-        disableOnInteraction: false
-        // pauseOnMouseEnter: true
-      },
+      // autoplay: {
+      //   delay: 4000,
+      //   disableOnInteraction: false
+      //   // pauseOnMouseEnter: true
+      // },
       pagination: {
         el: '.swiper-pagination',
         type: 'bullets'
@@ -274,16 +396,16 @@
       }
     });
 
-    $('.inner-contents').on({
-      mouseenter: function() {
-        mainSwiper.autoplay.stop();
-      },
-      mouseleave: function() {
-        if (!flag) {
-          mainSwiper.autoplay.start();
-        }
-      }
-    });
+    // $('.inner-contents').on({
+    //   mouseenter: function() {
+    //     mainSwiper.autoplay.stop();
+    //   },
+    //   mouseleave: function() {
+    //     if (!flag) {
+    //       mainSwiper.autoplay.start();
+    //     }
+    //   }
+    // });
 
     function setTransform(swiper) {
       $('.swiper-slide').removeClass('active');
